f9571315c9f9482c4fc5b56ab9cdc10879fd1492
GoktanBattal <goktanbattal@gmail.com>
2022-11-22 14:32:56 +0300

    half1 passed

--- src/index.js
+++ src/index.js
@@ -79,6 +79,7 @@ topContent.children[1].children[1].textContent = siteContent['main-content']['ab
 // Bottom Content
 
 const bottomContent = document.querySelector('.bottom-content')
+/* 
 bottomContent.children[0].children[0].textContent = siteContent['main-content']['services-h4']
 bottomContent.children[0].children[1].textContent = siteContent['main-content']['services-content']
 
@@ -87,3 +88,14 @@ bottomContent.children[1].children[1].textContent = siteContent['main-content'][
 
 bottomContent.children[2].children[0].textContent = siteContent['main-content']['vision-h4']
 bottomContent.children[2].children[1].textContent = siteContent['main-content']['vision-content']
+ */
+
+const h4sBottom = bottomContent.querySelectorAll('h4')
+h4sBottom[0].textContent = siteContent['main-content']['services-h4']
+h4sBottom[1].textContent = siteContent['main-content']['product-h4']
+h4sBottom[2].textContent = siteContent['main-content']['vision-h4']
+
+const psBottom = bottomContent.querySelectorAll('p')
+psBottom[0].textContent = siteContent['main-content']['services-content']
+psBottom[1].textContent = siteContent['main-content']['product-content']
+psBottom[2].textContent = siteContent['main-content']['vision-content']
